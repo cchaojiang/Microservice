2882486bf53215b464a12dc9f332494846f49105
jiangchao <cchaojiang@163.com>
2021-03-03 20:19:48 +0800

    WebApi创建

---
+++ order-api/order-api/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Common.Helper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace order_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        AppSettings _appset;
+        HelloController(AppSettings   appSettings  )
+        {
+            _appset = appSettings;
+        }
+        /// <summary>
+        /// Hello 请求
+        /// </summary>
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("Hello");
+        }
+    }
+}
---
+++ order-api/order-api/Startup.cs
@@ -0,0 +1,68 @@
+using Common.Helper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace order_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "order_api", Version = "v1" });
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, "order-api.xml");
+                c.IncludeXmlComments(xmlPath, false);//默认的第二个参数是false
+            });
+            services.AddSingleton(new AppSettings(Configuration));
+            var text = AppSettings.app(new string[] { "AppSettings", "ConnectionString" });
+            //Console.WriteLine($"ConnectionString:{text}");
+            //Console.ReadLine();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "order_api v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
